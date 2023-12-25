59cd0630b1374f77dc32e61fae551385d228bfd9
pairrz <dararat.buayim@gmail.com>
2023-12-25 22:05:03 +0700

    01

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F","W"};
+    cout <<"Press Enter 3 times to reveal your future.\n";
+    srand(time(0));
+    cin.get();
+    cin.get();
+    cin.get();
+    cout <<"You will get "<< grade[rand() % 9] <<" in this 261102.";
+    return 0;
+}
